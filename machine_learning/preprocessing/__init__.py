347f3905189cbac19f31524863f5962788479122
toshiki <sample@sample.com>
2020-09-12 18:54:02 +0900

    骨格を作ってみました

---
+++ machine_learning/preprocessing/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ['ALL_processing']
---
+++ machine_learning/preprocessing/processing.py
@@ -0,0 +1,37 @@
+
+
+class Image_for_ocr():
+    def __init__(self,path):
+        '''
+        path: 画像のパス
+        self.img :出力画像
+        self.size: 画像サイズ
+        self._preimg :元の画像
+        '''
+        pass
+        
+    def fit(self):
+        '''
+        self._binarization()
+        self._noise_remove()
+        self._canny()
+        ...
+        return self.img
+        みたいな...
+        '''
+        pass
+    
+
+    def _noise_remove(self):
+        '''ノイズを除去する'''
+        pass
+
+    def _binarization(self):
+        '''白黒にする'''
+        pass
+
+    def _canny():
+        '''canny法とか？'''
+        pass
+
+
