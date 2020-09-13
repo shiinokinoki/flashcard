9e49113cf7947b30859fc37762f2628ebf4d3957
椎木秀映 <shue@shiikishuueinoMacBook-Air.local>
2020-09-13 13:35:25 +0900

    モデル再構成に合わせた管理サイトの修正

--- flashcard/wordbook/admin.py
+++ flashcard/wordbook/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, NoteBook, Post
+from .models import User, NoteBook, Post
 
 
-admin.site.register(CustomUser)
+admin.site.register(User)
 admin.site.register(NoteBook)
 admin.site.register(Post)
