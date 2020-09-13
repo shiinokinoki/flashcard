3f026a0eafe6b4b1aaa542bbb929e4b2995958ad
椎木秀映 <shue@shiikishuueinoMacBook-Air.local>
2020-09-13 13:34:31 +0900

    モデルの再構成--認証機能の最適化

--- flashcard/wordbook/models.py
+++ flashcard/wordbook/models.py
@@ -1,36 +1,73 @@
 # Create your models here.
 from django.db import models
-from django.contrib.auth.models import AbstractUser,UnicodeUsernameValidator
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,UserManager
 from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 import uuid
 
+class UserManager(UserManager):
+    def _create_user(self, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-class CustomUser(AbstractUser):
-    username_validator = UnicodeUsernameValidator()
-    
-    username = models.CharField(
-        _('username'),
-        max_length=150,
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(_('email address'), unique=True)
+
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
         help_text=_(
-            'Required. 150 characters or fewer.'
-            ' Letters, digits and @/./+/-/_ only.'),
-        validators=[username_validator],
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
     )
-    email = models.EmailField(
-        _('email address'),
-        unique=True,
-        error_messages={
-            'unique': _("A user with that email address already exists."),
-        },
-    )
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = UserManager()
+
     EMAIL_FIELD = 'email'
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = []
 
-    class Meta(AbstractUser.Meta):
-        swappable = 'AUTH_USER_MODEL'
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
 
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this user."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
 
 
 class NoteBook(models.Model):
