bd69e1ebe0f43b2ec0619e62442f665688cc9730
椎木秀映 <shue@shiikishuueinoMacBook-Air.local>
2020-09-16 17:02:28 +0900

    問題作成機能

--- flashcard/wordbook/views.py
+++ flashcard/wordbook/views.py
@@ -25,41 +25,60 @@ class MyNotebookListView(generic.ListView):
         user = self.request.user
         return NoteBook.objects.filter(create_user=user)
 
-class MakeRegisterListView(LoginRequiredMixin, generic.ListView):
+# # class MakeRegisterListView(LoginRequiredMixin, generic.ListView):
+#     template_name = 'wordbook/register_list.html'
+#     model = Post
+#     def get_queryset(self):
+#         count =0
+#         path = './wordbook/data/json/dict_sample.json'
+#         scanned_dic = readjson(path=path)
+#         user = self.request.user
+#         for word in scanned_dic:
+#             for meaning in word['meaning']:
+#                 result, created = Post.objects.get_or_create(name = word['name'],meaning=meaning,create_user=user)
+#                 if created:
+#                     count +=1
+#         return Post.objects.filter(create_user=user).order_by('-date_joined')[:count]
+
+
+def makeregisterlist(request):
     template_name = 'wordbook/register_list.html'
     model = Post
-    def get_queryset(self):
-        count =0
-        path = './wordbook/data/json/dict_sample.json'
-        scanned_dic = readjson(path=path)
-        user = self.request.user
-        for word in scanned_dic:
-            for meaning in word['meaning']:
-                result, created = Post.objects.get_or_create(name = word['name'],meaning=meaning,create_user=user)
-                if created:
-                    count +=1
-        return Post.objects.filter(create_user=user).order_by('-date_joined')[:count]
+    count =0
+    path = './wordbook/data/json/dict_sample.json'
+    scanned_dic = readjson(path=path)
+    word_list =[]
+    for word in scanned_dic:
+        word_list.append(word['name'])
+        
+    context = {
+        'word_list':word_list,
+    }
+    return render(request, 'wordbook/register_list.html', context=context)
+        
 
-class GetChecklist(generic.ListView):
-    def post(self, request, *args, **kwargs):
-        user = self.request.user
-        checks_value = request.POST.getlist('checks[]')
-        checks_num = [int(n) for n in checks_value]
-        posts = Post.objects.filter(create_user=user).order_by('-date_joined')[:count]
-        for i,obj in enumerate(posts):
-            if i in checks_num:
-                continue
-            else:
-                obj.delete()
+# class GetChecklist(generic.ListView):
+#     def post(self, request, *args, **kwargs):
+#         user = self.request.user
+#         checks_value = request.POST.getlist('checks[]')
+#         checks_num = [int(n) for n in checks_value]
+#         posts = Post.objects.filter(create_user=user).order_by('-date_joined')[:count]
+#         for i,obj in enumerate(posts):
+#             if i in checks_num:
+#                 continue
+#             else:
+#                 obj.delete()
 
-def GetImage(request):
+def getimage(request):
     if request.method == 'POST':
         posted_img = request.FILES['image']
         cv2.imwrite('./wordbook/data/pict.jpg',posted_img)
         return redirect('registerlist')
 
-
-
+def makequestions(request):
+    
+    
+    return render(request, 'wordbook/questions.html',context=context)
 
 class GetAnswers(generic.ListView):
     def post(self, request, *args, **kwargs):
