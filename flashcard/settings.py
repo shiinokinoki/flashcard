e98c69c1066fa60ed571b1185df6ffeb5601b02e
toshiki <sample@sample.com>
2020-09-17 14:23:49 +0900

    deploy

--- flashcard/settings.py
+++ flashcard/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import mimetypes
 import logging
+
+from socket import gethostname
+hostname = gethostname()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -77,12 +81,26 @@ WSGI_APPLICATION = 'flashcard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# if "COMPUTER-NAME" in hostname:
+#     pass
+# デバッグ環境
+# DEBUG = True // 筆者はデバッグモードの切り替えもここでやってしまった
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# else:
+#     # 本番環境
+# DEBUG = False
+import dj_database_url
+db_from_env = dj_database_url.config()
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config()
 }
+ALLOWED_HOSTS = ['*']
 
 
 # Password validation
@@ -146,3 +164,7 @@ else:
         filename = '/my_log_file.log',
         filemode = 'a'
     )
+
+
+import django_heroku
+django_heroku.settings(locals())
